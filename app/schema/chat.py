ebb6d08f4762ca4621b3cbbc2588365bfad4d30a
minhyuklee <leeminhyuk12@naver.com>
2025-08-21 15:48:19 +0900

    UPDATE

--- app/schema/chat.py
+++ app/schema/chat.py
@@ -1,17 +1,17 @@
 from re import S
-from pydantic import BaseModel, Field
+from ._base import CamelCaseModel
 from typing import Optional
 
-class ChatRequest(BaseModel):
+class ChatRequest(CamelCaseModel):
     user_message: str
     chat_id: str 
     company: str
     user_id: Optional[str] = None
 
-class NewChatRequest(BaseModel):
+class NewChatRequest(CamelCaseModel):
     user_message: str
     company: str
     user_id: Optional[str] = None
 
-class CreatePlanRequest(BaseModel):
+class CreatePlanRequest(CamelCaseModel):
     chat_id: str
